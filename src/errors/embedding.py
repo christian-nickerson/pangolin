8ce6d4f06fc3019fbac2f955a157a16a6a23f7a5
Christian Nickerson <christian.nickerson@faculty.ai>
2024-08-01 11:42:04 +0100

    embedding errors added

---
+++ src/errors/embedding.py
@@ -0,0 +1,15 @@
+class ModelRemoteImportError(Exception):
+    def __init__(
+        self,
+        model_name: str,
+        repository: str,
+    ) -> None:
+        """Exception raised when model failed to import from remote repository
+
+        :param model_name: name of model that failed to import
+        :param site: name of repository used to import model
+        :param message: explanation of the error, defaults to "{model_name} could not be found"
+        """
+        self.message = "{model_name} not found on {repository}"
+        self.message = self.message.format(model_name, repository)
+        super().__init__(self.message)
