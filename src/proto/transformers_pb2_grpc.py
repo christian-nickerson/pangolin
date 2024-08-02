41343548f7d4ef0a03cfe65f20f4a8a371e37863
Christian Nickerson <christian.nickerson@faculty.ai>
2024-08-02 13:13:03 +0100

    mypy pre-commit protobud fix

--- src/proto/transformers_pb2_grpc.py
+++ src/proto/transformers_pb2_grpc.py
@@ -6,7 +6,7 @@ import warnings
 
 import transformers_pb2 as transformers__pb2
 
-GRPC_GENERATED_VERSION = "1.65.1"
+GRPC_GENERATED_VERSION = "1.65.2"
 GRPC_VERSION = grpc.__version__
 EXPECTED_ERROR_RELEASE = "1.66.0"
 SCHEDULED_RELEASE_DATE = "August 6, 2024"
