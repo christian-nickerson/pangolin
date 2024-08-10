c88bbb8fc91aabfaa9979fb7779a5e137edfbb69
Christian Nickerson <christian.nickerson@faculty.ai>
2024-08-10 16:00:19 +0100

    testify added

--- api/internal/configs/settings_test.go
+++ api/internal/configs/settings_test.go
@@ -4,20 +4,16 @@ import (
 	"testing"
 
 	_ "github.com/christian-nickerson/pangolin/api/testing"
+	"github.com/stretchr/testify/assert"
 )
 
 // test load config can read the default toml file
 // and has required default values
 func TestLoadConfig(t *testing.T) {
+	assert := assert.New(t)
 	settings, err := LoadSettings("settings")
-	if err != nil {
-		t.Fatalf("failed to load config: %v", err)
-	}
 
-	if settings.EmbeddingServer.Name == "" {
-		t.Fatalf("embeddinger-server.name should have a default value")
-	}
-	if settings.EmbeddingServer.Port != 50051 {
-		t.Fatalf("embeddinger-server.port should have a default of 50051")
-	}
+	assert.Nil(err, "failed to load config: %v", err)
+	assert.NotEqual(settings.EmbeddingServer.Name, "", "embedding-server.name should have a default value")
+	assert.Equal(settings.EmbeddingServer.Port, 50051, "embedding-server.port should have a default of 50051")
 }
