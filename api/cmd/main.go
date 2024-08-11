79fe3888d1feca84ae8484d6cc58b7c6e83acade
Christian Nickerson <christian.nickerson@faculty.ai>
2024-08-11 12:51:00 +0100

    fiber app added

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"strconv"
+
+	"github.com/goccy/go-json"
+
+	"github.com/christian-nickerson/pangolin/api/internal/configs"
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	// load settings
+	settings, err := configs.LoadSettings("settings")
+	if err != nil {
+		log.Fatal("Loading settings failed:", err)
+	}
+
+	// set up fiber app
+	app := fiber.New(fiber.Config{
+		JSONEncoder: json.Marshal,
+		JSONDecoder: json.Unmarshal,
+	})
+
+	// run app
+	app.Listen(":" + strconv.Itoa(settings.Server.API.Port))
 }
