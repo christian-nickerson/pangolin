9c5e60bb5b836672c3ce09308a04fe3d11d3a1d3
Christian Nickerson <christian_nickerson@hotmail.com>
2024-08-13 15:27:53 +0100

    grpc embedding client added

---
+++ api/internal/embedding/client.go
@@ -0,0 +1,53 @@
+package embeddings
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"google.golang.org/grpc"
+
+	"github.com/christian-nickerson/pangolin/api/internal/proto"
+)
+
+var Client proto.EmbeddingsClient
+var Conn *grpc.ClientConn
+
+func Connect(address string) {
+	var err error
+
+	Conn, err = grpc.NewClient(address)
+	if err != nil {
+		log.Fatal("Failed to connect to client:", err)
+	}
+
+	Client = proto.NewEmbeddingsClient(Conn)
+}
+
+func Inference(text []string, modelName string) []*proto.Vector {
+	// timeout after 5 mins
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
+	defer cancel()
+
+	// call model
+	response, err := Client.Inference(ctx, &proto.InferenceRequest{Text: text, ModelName: modelName})
+	if err != nil {
+		log.Fatalf("Client.Inference failed on model %v: %v", modelName, err)
+	}
+
+	return response.Embeddings
+}
+
+func ModelList() []string {
+	// timeout after 5 seconds
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// call model
+	response, err := Client.ModelList(ctx, &proto.ModelListRequest{})
+	if err != nil {
+		log.Fatalf("Client.ModelList failed: %v", err)
+	}
+
+	return response.ModelNames
+}
