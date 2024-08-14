7afdc47062e5986d384b85d14eb7ab7a086bc898
Christian Nickerson <christian_nickerson@hotmail.com>
2024-08-14 18:58:46 +0100

    databases model added

---
+++ api/internal/models/databases.go
@@ -0,0 +1,16 @@
+package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Database struct {
+	ID          uint           `json:"id" gorm:"primaryKey;autoIncrement;unique"`
+	Name        string         `json:"name" gorm:"unique" validate:"required"`
+	Description *string        `json:"description"`
+	CreatedAt   time.Time      `json:"createdAt"`
+	UpdatedAt   time.Time      `json:"updatedAt"`
+	DeletedAt   gorm.DeletedAt `json:"deletedAt" gorm:"index"`
+}
